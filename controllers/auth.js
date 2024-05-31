521e576befe8cae2a2cd89939dc0ace9f18b5be1
ciro brachetta <cirobrachetta@gmail.com>
2024-05-31 20:23:35 -0300

    andando

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,16 +12,18 @@ const DataBase = mysql.createConnection({
 });
 
 exports.register = (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
 
     const { name, email, password, passwordConfirm } = req.body;
-
+try {
     DataBase.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => { 
         //de aca no entiendo el "async" ni tampoco de donde saca el results o el error
+        console.log("resultado: ", results.code);
+        console.log("salida de error: " + error);
         if(error){
             console.log(error);
         }
-        if(results.lenght > 0) {
+        if(results.code == 'ER_DUP_ENTRY') {
             return res.render('register', {
                 message: 'That Email is already in use'
             })
@@ -35,18 +37,61 @@ exports.register = (req, res) => {
         let hashedPassword = await bcrypt.hash(password, 8); //aca no entiendo ni el "let" ni el "await"
         console.log(hashedPassword);
 
-        DataBase.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword}, (error, results)=>{
-            if(error){
-                console.log(error);
-            } else {
-                console.log(results);
-
+         DataBase.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword}, (error, results)=>{
+        //     console.log("salida error: ", results);
+             if(error){
                 return res.render('register', {
-                    message: "User registerd"
-                });
-            }
-        })
+                    message: ' That Email is already in use'
+                })
+                console.log(error.errno, "Error 3");
+            } else {
+                 console.log(results);
+
+                 return res.render('register', {
+                     message: "User registerd"
+                 });
+             }
+         })
         //que es exactamente un query de SQL?
 
     })
+} catch (error) {
+    console.log(error, "error de catch");
+}
+    // DataBase.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => { 
+    //     //de aca no entiendo el "async" ni tampoco de donde saca el results o el error
+    //     console.log("resultado: ", results.code);
+    //     console.log("salida de error: " + error);
+    //     if(error){
+    //         console.log(error);
+    //     }
+    //     if(results.code == 'ER_DUP_ENTRY') {
+    //         return res.render('register', {
+    //             message: 'That Email is already in use'
+    //         })
+    //     }
+    //     else if( password !== passwordConfirm){
+    //         return res.render('register', {
+    //             message: "The passwords do not match"
+    //         })
+    //     }
+
+    //     let hashedPassword = await bcrypt.hash(password, 8); //aca no entiendo ni el "let" ni el "await"
+    //     console.log(hashedPassword);
+
+    //     DataBase.query('INSERT INTO users SET ?', {name: name, email: email, password: hashedPassword}, (error, results)=>{
+    //         console.log("salida error: ", results);
+    //         if(error){
+    //             console.log(error);
+    //         } else {
+    //             console.log(results);
+
+    //             return res.render('register', {
+    //                 message: "User registerd"
+    //             });
+    //         }
+    //     })
+    //     //que es exactamente un query de SQL?
+
+    // })
 }
